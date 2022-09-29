2931c69d050d8b6405918a14023c7d8c264b4b6b
aweisenauer <112560452+aweisenauer@users.noreply.github.com>
2022-09-29 15:44:57 -0400

    playdate form

---
+++ capstone/vue/src/components/SchedulePlayDateForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <form class="form">
+    <h2>Schedule a Play Date:</h2>
+    <div class="fields">
+      <label for="address-1">Address line 1:</label>
+      <input type="text" required v-model="addressOne"/>
+      <label for="address-2">Address line 2:</label>
+      <input type="text" v-model="addressTwo"/>
+      <label for="city">City:</label>
+      <input type="text" required v-model="city"/>
+      <label for="state">State:</label>
+      <input type="text" required v-model="state"/>
+      <label for="zip">Zip:</label>
+      <input type="text" required v-model="zip"/>
+      <select v-model="hostPet">
+          <option disabled value="">Hosting Pet</option>
+          <option>Doggo</option>
+          <option>Doggy</option>
+          <option>Sir Patrick Stewart</option>
+      </select>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+data(){
+    return {
+        addressOne: '',
+        addressTwo: '',
+        city: '',
+        state: '',
+        zip: '',
+        hostPet: ''
+
+    }
+}
+
+
+
+};
+</script>
+
+<style>
+</style>
