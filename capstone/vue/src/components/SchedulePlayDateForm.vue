6dc089349137994ecaa4eb7d66cfe1283d3cfb16
aweisenauer <112560452+aweisenauer@users.noreply.github.com>
2022-10-03 11:49:26 -0400

    create Platdate form

--- capstone/vue/src/components/SchedulePlayDateForm.vue
+++ capstone/vue/src/components/SchedulePlayDateForm.vue
@@ -2,6 +2,14 @@
   <div class="play-date">
     <form id="play-date-form" class="form" @submit.prevent="submitForm">
       <div class="form-element">
+        <label for="location-name">Location Name</label>
+        <input
+          id="location-name"
+          type="text"
+          required
+          v-model="locationName"
+          placeholder="Location Name"
+        />
         <label for="add-1">Address </label>
         <input
           id="add-1"
@@ -13,12 +21,23 @@
       </div>
       <div class="form-element">
         <label for="add-2">Address </label>
-        <input id="add-2" type="text" v-model="addressTwo" placeholder="Address 2" />
+        <input
+          id="add-2"
+          type="text"
+          v-model="addressTwo"
+          placeholder="Address 2"
+        />
       </div>
       <div class="form-element">
         <label for="city">City </label>
-        <input id="city" type="text" required v-model="city" placeholder="City" />
-      </div>  
+        <input
+          id="city"
+          type="text"
+          required
+          v-model="city"
+          placeholder="City"
+        />
+      </div>
       <div class="form-element">
         <label for="search-state">State </label>
         <input
@@ -28,20 +47,31 @@
           v-model="state"
           placeholder="State"
         />
-      </div>  
+      </div>
       <div class="form-element">
         <label for="zip-code">Zip Code </label>
-        <input id="zip-code" type="text" required v-model="zip" placeholder="Zip" />
-      </div> 
+        <input
+          id="zip-code"
+          type="text"
+          required
+          v-model="zip"
+          placeholder="Zip"
+        />
+      </div>
       <div class="form-element">
         <label for="pets">Hosting Pet </label>
-        <select id="pets" placeholder="Hosting Pet" required v-model="hostPetDropDown">
-          <option value="" disabled> --- </option>
+        <select
+          id="pets"
+          placeholder="Hosting Pet"
+          required
+          v-model="chosenPetId"
+        >
+          <option value="" disabled>---</option>
           <option :value="n.petId" v-for="n in filterPets" :key="n">
             {{ n.petName }}
           </option>
         </select>
-      </div> 
+      </div>
       <div class="form-element">
         <label for="time">Time </label>
         <input
@@ -53,47 +83,58 @@
           v-model="time"
           required
         />
-      </div>  
+        <div class="form-element">
+          <label for="date">Date</label>
+          <input type="date" required v-model="date" />
+        </div>
+      </div>
 
       <button id="submit-btn" type="submit">Add Play Date</button>
+      <h1>{{ playDateCreated }}</h1>
+      <h1>{{ time }}</h1>
+      <h1>{{ date }}</h1>
+      <h1>{{ dateTime }}</h1>
     </form>
   </div>
-  
 </template>
 
 <script>
 import geocodeService from "../services/GeocodeService";
 import mapService from "../services/MapService";
 import playDateService from "../services/PlayDateService";
-import petService from "../services/petService"
+import petService from "../services/petService";
+import moment from "moment";
 export default {
   data() {
     return {
-      formInfo: {
+      //formInfo
+      locationName: "",
       addressOne: "",
       addressTwo: "",
       city: "",
       state: "",
       zip: "",
-       latitude: "",
+      latitude: "",
       longitude: "",
-      time: ''
-},
-     
+      chosenPetId: "",
+      time: "",
+      date: "",
+      //end of form
+      dateTime: "",
+      playDateCreated: false,
       couldNotFindAddress: false,
       currentOwnerId: 0,
       pets: [],
     };
   },
   methods: {
-    resetForm(){
+    resetForm() {
       this.formInfo = {};
     },
-    sendToHome(){
+    sendToHome() {
       this.resetForm();
       this.$router.push("/");
     },
-
 
     submitForm() {
       this.getAddressCoords();
@@ -102,24 +143,48 @@ export default {
       }
       let locationId = this.checkForLocation();
       if (locationId == -1) {
-        
         //create new location in database
-        mapService.createLocation();
+        let location = {
+          locationId: 0,
+          locationName: this.locationName,
+          address1: this.addressOne,
+          address2: this.addressTwo,
+          city: this.city,
+          stateAbbreviation: this.state,
+          zipCode: this.zip,
+          latitude: parseFloat(this.latitude),
+          longitude: parseFloat(this.longitude),
+        };
+        mapService
+          .createLocation(location)
+          .then((response) => {
+            locationId = response.data.locationId;
+            this.storePlayDate(locationId);
+          })
+          .catch((error) => {
+            this.playDateCreated = error.response;
+          });
+      } else {
+        this.storePlayDate(locationId);
+      }
+    },
 
-
-        //get new locationId
-        mapService.getLocationById(this.locationId)
-      
+    storePlayDate(locationId) {
       //add locationId to new playdate
-
-
-
+      this.dateTime = moment(this.date + " " + this.time);
+      const newPlayDate = {
+        playDateId: 0,
+        hostPetId: this.chosenPetId,
+        attendingPetIds: [],
+        status: "pending",
+        locationId: locationId,
+        playDateTimeStamp: this.dateTime,
+      };
       //create new playdate with found locationId
-      playDateService.createPlayDate();
-
-
-
-    }},
+      playDateService.createPlayDate(newPlayDate).then(() => {
+        this.playDateCreated = true;
+      });
+    },
 
     getAddressCoords() {
       let searchAddress =
@@ -180,89 +245,88 @@ export default {
       }
     },
   },
-   computed: {
-          filterPets() {
-            return this.pets.filter(pet => pet.ownerId == this.currentOwnerId);
-          }
-        },
-        created() {
-          petService.getAllPets().then(res => {
-            if (res.status === 200) {
-              this.pets = res.data;
-            }
-          });
-          this.currentOwnerId = this.$store.state.user.id;
-        }
+  computed: {
+    filterPets() {
+      return this.pets.filter((pet) => pet.ownerId == this.currentOwnerId);
+    },
+  },
+  created() {
+    petService.getAllPets().then((res) => {
+      if (res.status === 200) {
+        this.pets = res.data;
+      }
+    });
+    this.currentOwnerId = this.$store.state.user.id;
+  },
 };
 </script>
 
 <style scoped>
-  .play-date {
-    max-width: 80%;
-    margin: auto;
-    font-family: 'Montserrat', sans-serif;
-    color: #2C3333;
-  }
+.play-date {
+  max-width: 80%;
+  margin: auto;
+  font-family: "Montserrat", sans-serif;
+  color: #2c3333;
+}
 
-  #play-date-form{
-    font-size: 15px;
-  }
+#play-date-form {
+  font-size: 15px;
+}
 
-  .form-element {
-    margin: 15px 0;
-    font-size: 1.3em;
-  }
+.form-element {
+  margin: 15px 0;
+  font-size: 1.3em;
+}
 
+label {
+  display: block;
+}
+
+input[type="text"] {
+  line-height: 25px;
+  font-size: 18px;
+  border-radius: 5px;
+  border: 1px solid black;
+}
+
+select {
+  font-size: 20px;
+}
+
+#submit-btn {
+  background-color: #676fa3de;
+  font-family: monospace;
+  font-size: 18px;
+  color: white;
+  width: 100%;
+  border: none;
+  border-radius: 5px;
+  height: 35px;
+  letter-spacing: 2px;
+  transition: all 0.3s ease;
+  max-width: 540px;
+  margin-bottom: 30px;
+}
+
+#submit-btn:hover {
+  background-color: #c1d5fc;
+  color: #2c3333;
+}
+
+@media (min-width: 670px) {
   label {
-    display: block;
+    display: inline;
+  }
+}
+
+@media (min-width: 920px) {
+  .play-date {
+    text-align: center;
   }
 
-  input[type="text"] {
-    line-height: 25px;
-    font-size: 18px;
-    border-radius: 5px;
-    border: 1px solid black;
+  #play-date-form {
+    display: inline-block;
+    text-align: left;
   }
-
-  select {
-    font-size: 20px;
-  }
-
-  #submit-btn {
-    background-color: #676fa3de;
-    font-family: monospace;
-    font-size: 18px;
-    color: white;
-    width: 100%;
-    border: none;
-    border-radius: 5px;
-    height: 35px;
-    letter-spacing: 2px;
-    transition: all .3s ease;
-    max-width: 540px;
-    margin-bottom: 30px;
-  }
-
-  #submit-btn:hover {
-    background-color: #c1d5fc;
-    color: #2C3333;
-  }
-
-   @media(min-width:670px) {
-      label {
-        display: inline;
-      }
-    }
-
-  @media(min-width:920px) {
-    .play-date {
-      text-align: center;
-    }
-    
-    #play-date-form {
-      display: inline-block;
-      text-align: left;
-        
-    }
-  }
+}
 </style>
