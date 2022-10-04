949566c951dd356ffa62b217cd196d3a97414fc6
Kat Fleck <jfleck01@gmail.com>
2022-10-04 15:42:16 -0400

    Completed firebase storage setup for uploading pictures

--- capstone/vue/src/services/imageService.js
+++ capstone/vue/src/services/imageService.js
@@ -1,17 +1,32 @@
 import { fireDb, fireStorage } from '@/firebase';
-import { ref as dbRef, set } from 'firebase/database';
+import { ref as dbRef, set, onValue } from 'firebase/database';
 import { ref as storeRef, uploadBytes, getDownloadURL } from 'firebase/storage'
 
 export default {
   uploadImage(petImage) {
-    return set(dbRef(fireDb, 'pet_pictures/'), petImage);
+    return set(dbRef(fireDb, `pet_pictures/${petImage.id}`), petImage);
   },
-  storeImage(petPicture) {
-    let imageRef = storeRef(fireStorage, 'pet_pictures/' + petPicture.name)
+  storeImage(petPicture, petId) {
+    let imageRef = storeRef(
+      fireStorage,
+      `pet_pictures/${petId}${petPicture.name.slice(
+        petPicture.name.lastIndexOf('.')
+      )}`);
     uploadBytes(imageRef, petPicture).then(() => {
       getDownloadURL(imageRef).then(url => {
-        return url;
-      })
+        const petImage = {
+          id: petId,
+          url: url
+        };
+        this.uploadImage(petImage);
+      });
     });
   },
+  getImageUrlById(id) {
+    onValue(dbRef(fireDb, `pet_pictures/${id}`), (snapshot) => {
+      return snapshot.val().url;
+    }, {
+      onlyOnce: true
+    });
+  }
 }
